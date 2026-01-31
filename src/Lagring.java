72f436b20a854aff8554d8decd3cf44adf578efa
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2026-01-31 13:05:33 +0100

    Fixar logiska buggar

--- src/Lagring.java
+++ src/Lagring.java
@@ -3,10 +3,10 @@ public abstract class Lagring {
     private double pris;
 
     protected Lagring(double förrådsArea, double pris) {
-        if (förrådsArea <= 0 && förrådsArea > 150) {
+        if (förrådsArea <= 0 || förrådsArea > 150) {
             throw new IllegalArgumentException("Välj snälla tillåtna värden");
         }
-        if (pris <= 0 && pris > 500000) {
+        if (pris <= 0 || pris > 500000) {
             throw new IllegalArgumentException("Välj snälla tillåtna värden");
         }
 
